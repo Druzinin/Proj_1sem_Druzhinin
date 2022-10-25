a681ad1f73d38c2ec8e4bb9b2e7ae8c37d12933d
Abyka12 <druzinin200511@gmail.com>
2022-10-25 18:44:11 +0300

    commit work

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,13 +1,13 @@
-from random import randint
+from random import randint  # импортирование функции random
 n = randint(1000, 9999)
 print(n)
 
-a = n // 1000
+a = n // 1000  # разделение числа на отдельные цифры
 b = n % 1000 // 100
 c = n % 100 // 10
 d = n % 10
 
-if a == b or a == c or a == d or b == c or b == d or c == d:
-    print('Есть повторяющиеся цифры')
+if a == b or a == c or a == d or b == c or b == d or c == d:  # проверка на одинаковые цифры
+    print('Есть одинаковые цифры')
 else:
-    print('Нет повторяющихся цифр')
+    print('Нет одинаковых цифр')
