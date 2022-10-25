1979c1416d04d79efd8c27d88e33cc20b5a5f547
Abyka12 <druzinin200511@gmail.com>
2022-10-25 17:41:10 +0300

    add pz_5_2

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,1 +1,13 @@
-print(' ')
+from random import randint
+n = randint(1000, 9999)
+print(n)
+
+a = n // 1000
+b = n % 1000 // 100
+c = n % 100 // 10
+d = n % 10
+
+if a == b or a == c or a == d or b == c or b == d or c == d:
+    print('Есть повторяющиеся цифры')
+else:
+    print('Нет повторяющихся цифр')
