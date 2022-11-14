ec67564fbdbfaa78460a8090ead32a1f4ccc651f
Druzinin <druzinin200511@gmail.com>
2022-11-14 22:12:19 +0300

    Commit PZ

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,5 +1,5 @@
 # Дан список A размера N. Найти минимальный элемент из его элементов с четными номерами: А2, А4, А6, …
-from random import randint  # Импортирование модуля randint
+from random import randint  # Импортирование модуля random
 
 n = input('Введите N: ')  # Ввод данных
 lst = []
@@ -15,9 +15,9 @@ while type(n) != int:  # Обработка исключений
 a = [randint(0, 9) for _ in range(n)]  # Генерация списка
 print('Исходный список:', a)
 
-for i in a:  # Добавление элементов с четными индексами
+for i in a:
     if k % 2 == 0:
-        lst.append(i)
+        lst.append(i)  # Добавление элементов с четными индексами
     k += 1
 
-print(min(lst))
+print(min(lst))  # Вывод минимального элемента
