268ae92d0df89f8909c198fec0828185f7598144
Druzinin <druzinin200511@gmail.com>
2022-11-14 19:10:11 +0300

    Commit PZ

---
+++ __init__.py
@@ -0,0 +1,36 @@
+from random import randint  # Импортирование модуля randint
+N = input('Введите N: ')  # Ввод данных
+
+while type(N) != int:  # Обработка исключений
+    try:
+        N = int(N)
+    except ValueError:
+        print('Неправильный ввод')
+        N = input('Введите N: ')
+
+a = [randint(0, 9) for i in range(N)]  # Генерация списка
+print(a)
+
+k1, k2 = 0, 0
+flag = 1
+
+for i in range(1, N):
+    if a[i] < a[i - 1]:  # Текущее число меньше предыдущего
+        if flag:
+            k1 += 1
+            flag = 0
+    else:
+        flag = 1
+
+flag = 1
+
+for i in range(1, N):
+    if a[i] > a[i - 1]:  # Текущее число меньше предыдущего
+        if flag:
+            k2 += 1
+            flag = 0
+    else:
+        flag = 1
+
+k = k1 + k2
+print(k)  # Вывод результата
