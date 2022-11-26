ed694340ea9f3e3a511da8fd447bc091e023e1a6
Abyka12 <antondvachevsky@yandex.ru>
2022-11-26 10:40:19 +0300

    commit work

--- PZ_9/PZ_9_1.py
+++ PZ_9/PZ_9_1.py
@@ -1,3 +1,4 @@
-d = dict(keyboard='клавиатура', mouse='мышь', display='монитор', printer='принтер', high='', )
-s = input('Введите слово на английском: ')
+d = dict(keyboard='клавиатура', mouse='мышь', display='монитор', printer='принтер', low='низкий',
+         high='высокий', cheap='дешевый', expensive='дорогой', quality='качество', quiet='тихий')
+s = input('Введите слово на английском: ').lower()
 print(d[s] if s in d else 'Нет такого слова в словаре')
