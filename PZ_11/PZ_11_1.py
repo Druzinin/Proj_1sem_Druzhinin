5b934656475b7735841556a245bd24921fca5028
Druzinin <druzinin200511@gmail.com>
2023-01-13 23:21:07 +0300

    Commit PZ

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -18,11 +18,11 @@ print(*l1, file=f1)  # Добавление последовательности
 print(*l2, file=f2)
 f1.close(), f2.close()  # Закрытие файлов
 
-f3 = open('file 3.txt', 'w')
+f3 = open('file 3.txt', 'w')  # Создание нового файла
 f1, f2 = open('file 1.txt').readline().split(), open('file 2.txt').readline().split()  # Считывание первых строк файлов
-f1, f2 = list(map(int, f1)), list(map(int, f2))
+f1, f2 = list(map(int, f1)), list(map(int, f2))  # Преобразование типа 'str' в 'int'
 
-print('1) Элементы первого и второго файлов:', f1, f2, file=f3)  # Записывание данных в файлы
+print('1) Элементы первого и второго файлов:', f1, f2, file=f3)  # Записывание данных в файл
 print('2) Элементы первого файла, присутствующие во втором:', [i for i in f1 if i in f2], file=f3)
 print('3) Элементы второго файла, присутствующие в первом:', [i for i in f2 if i in f1], file=f3)
 print('4) Количество элементов:', len(f1 + f2), file=f3)
