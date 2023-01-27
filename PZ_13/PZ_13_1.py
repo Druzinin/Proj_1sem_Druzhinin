9bbd473e197630a458b0fca82fe3634eca1920f4
Druzinin <druzinin200511@gmail.com>
2023-01-27 20:17:36 +0300

    Commit PZ

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -1,12 +1,15 @@
-from random import randint
-n, m = map(int, input('Введите размер матрицы (два целых числа через пробел): ').split())
-matrix = [[randint(-10, 10) for _ in range(m)] for _ in range(n)]
+# В матрице найти среднее арифметическое положительных элементов, кратных 3.
+
+from random import randint  # Импортирование модуля
+n, m = map(int, input('Введите размер матрицы (два целых числа через пробел): ').split())  # Ввод данных
+matrix = [[randint(-10, 10) for _ in range(m)] for _ in range(n)]  # Генерация матрицы
 print('Исходная матрица: ', *matrix, sep='\n')
 
 array = []
-for i in range(n):
+for i in range(n):  # Циклы для перебора элементов матрицы
     for j in range(m):
         if matrix[i][j] > 0 and matrix[i][j] % 3 == 0:
-            array.append(matrix[i][j])
+            array.append(matrix[i][j])  # Добавление в массив элементов, соответствующих условию
 
-print('\n', 'Среднее арифметическое чисел: ', sum(array) / len(array))
+print(f'\nСписок: {array}\nСреднее арифметическое элементов: {sum(array) / len(array)}' if array else
+      '\nНет подходящих элементов')  # Вывод результата
--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,13 +1,15 @@
-from random import randint
-n, m = map(int, input('Введите размер матрицы (два целых числа через пробел): ').split())
-N = int(input('Введите номер строки: '))
-matrix = [[randint(-10, 10) for _ in range(m)] for _ in range(n)]
+# В матрице элементы строки N (N задать с клавиатуры) увеличить на 3.
+
+from random import randint  # Импортирование модуля
+n, m = map(int, input('Введите размер матрицы (два целых числа через пробел): ').split())  # Ввод данных
+N = int(input(f'Введите номер строки (<= {n}): '))
+matrix = [[randint(-10, 10) for _ in range(m)] for _ in range(n)]  # Генерация матрицы
 print('Исходная матрица: ', *matrix, sep='\n')
 
-for i in range(n):
+for i in range(n):  # Циклы для перебора элементов матрицы
     for j in range(m):
         if i == N - 1:
-            matrix[i][j] += 3
+            matrix[i][j] += 3  # Увеличение элементов в строке N на 3
 
-print('Измененная матрица: ')
+print('\nИзмененная матрица: ')  # Вывод результата
 print(*matrix, sep='\n')
