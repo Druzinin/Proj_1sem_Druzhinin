ff35f8150d2e9984beb41076c0bcc47320eb00b1
Druzinin <druzinin200511@gmail.com>
2023-02-04 17:12:13 +0300

    Commit PZ

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,2 +1,7 @@
-f = open('Dostoevsky.txt').readlines()
-print(f)
+f = open('Dostoevsky.txt', encoding='utf-8').read()
+
+index = f.find('1857 год')
+s = f[index:f.rfind('\n', index, f.find('гг.', index))]
+new_file = open('new_text_file.txt', 'w')
+print(s, file=new_file)
+new_file.close()
