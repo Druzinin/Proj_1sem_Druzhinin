feea3eac51ce6ba78481753c29d462708c6983f1
Druzinin <druzinin200511@gmail.com>
2023-02-20 23:04:11 +0300

    Commit PZ

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,10 +1,11 @@
 # Из исходного текстового файла (Dostoevsky.txt) выбрать блок информации за 1857 год и поместить ее в новый
 # текстовый файл.
-from re import findall
-f = open('Dostoevsky.txt', encoding='utf-8').read()
-s = findall(r'1857 год \n([\w\W]+)\n1860', f)
-print(s[0])
+from re import findall, M, S
+year = '1857'
 
-new_file = open('new_text_file.txt', 'w', encoding='utf-8')
-print(s[0], file=new_file)
-new_file.close()
+with open('Dostoevsky.txt', encoding='utf-8') as f:
+    s, = findall(year + r".*?\n(.+?)\n^\d{4}", f.read(), M | S)
+    print(s)
+
+with open('new_text_file.txt', 'w', encoding='utf-8') as new_file:
+    print(s, file=new_file)
