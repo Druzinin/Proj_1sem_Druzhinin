41da6e04dad6f9e3e1639fd98e7f1f5b1598f79c
Druzinin <114361040+Druzinin@users.noreply.github.com>
2023-03-13 01:52:24 +0300

    Commit PZ

--- PZ_16/main.py
+++ PZ_16/main.py
@@ -2,6 +2,6 @@ from figures import circle_perimeter, circle_area
 from figures import square_perimeter, square_area
 from figures import triangle_perimeter, triangle_area
 
-print(circle_perimeter(10), circle_area())
-print(square_perimeter(), square_area(10))
-print(triangle_perimeter(1, 2, 3), triangle_area())
+print(circle_perimeter(), circle_area(4))
+print(square_perimeter(), square_area())
+print(triangle_perimeter(), triangle_area(3, 4, 5))
