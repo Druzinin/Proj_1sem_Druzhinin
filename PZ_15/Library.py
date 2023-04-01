42405bb0ffb1f436aa292ab7b796cad00af53470
Druzinin <druzinin200511@gmail.com>
2023-04-01 23:24:16 +0300

    Commit PZ

--- PZ_15/Library.py
+++ PZ_15/Library.py
@@ -1,15 +1,14 @@
 import sqlite3 as sq
-# from datetime import date
 from data import *
 
 with sq.connect('Library.sqlite') as con:
     con.execute("PRAGMA foreign_keys = ON;")
     cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS author_book;")
     cur.execute("DROP TABLE IF EXISTS authors;")
     cur.execute("DROP TABLE IF EXISTS books;")
     cur.execute("DROP TABLE IF EXISTS sections;")
     cur.execute("DROP TABLE IF EXISTS publishing;")
-    cur.execute("DROP TABLE IF EXISTS author_book;")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS authors (
     id_author INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,16 +19,16 @@ with sq.connect('Library.sqlite') as con:
     cur.execute("""CREATE TABLE IF NOT EXISTS books (
     id_books INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    year_publishing TEXT,
+    year_publishing INTEGER,
     storage TEXT,
-    id_section TEXT
+    section TEXT
     REFERENCES sections ON DELETE CASCADE ON UPDATE CASCADE,
-    id_publishing TEXT
+    id_publishing INTEGER
     REFERENCES publishing ON DELETE CASCADE ON UPDATE CASCADE
     );""")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS sections (
-    id_section INTEGER PRIMARY KEY AUTOINCREMENT
+    section TEXT PRIMARY KEY
     );""")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS publishing (
@@ -49,17 +48,19 @@ with sq.connect('Library.sqlite') as con:
 with sq.connect('Library.sqlite') as con:
     con.execute("PRAGMA foreign_keys = ON;")
     cur = con.cursor()
-    cur.execute("INSERT INTO authors VALUES (?, ?, ?)", info_authors)
-    cur.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?)", info_books)
-    cur.execute("INSERT INTO sections VALUES (?)", info_sections)
-    cur.execute("INSERT INTO publishing VALUES (?, ?)", info_publishing)
-    cur.execute("INSERT INTO author_book VALUES (?, ?, ?)", info_author_book)
+    cur.executemany("INSERT INTO authors VALUES (?, ?, ?)", info_authors)
+    cur.executemany("INSERT INTO sections VALUES (?)", info_sections)
+    cur.executemany("INSERT INTO publishing VALUES (?, ?)", info_publishing)
+    cur.executemany("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?)", info_books)
+    cur.executemany("INSERT INTO author_book VALUES (?, ?, ?)", info_author_book)
 
 
 with sq.connect('Library.sqlite') as con:
     cur = con.cursor()
-    cur.execute("SELECT name FROM books ORDER BY year_publishing")
-    cur.execute("""SELECT name FROM books INNER JOIN
-    author_book ON books.id_books = id_book INNER JOIN
-    id_author ON authors.id_author = id_author_book
-    """)
+    print(*cur.execute("SELECT name, year_publishing FROM books ORDER BY year_publishing"))
+    print(*cur.execute("""SELECT name FROM books INNER JOIN
+    author_book ON books.id_books = id_book
+    """))
+    print(*cur.execute("SELECT * FROM books"))
+    print(*cur.execute("SELECT surname FROM authors ORDER BY surname"))
+    print(*cur.execute("SELECT * FROM books"))
