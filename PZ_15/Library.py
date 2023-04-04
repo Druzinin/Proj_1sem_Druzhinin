1e897ed9faab107453e317fcb4ae05dbe5d45c77
Druzinin <druzinin200511@gmail.com>
2023-04-04 21:45:19 +0300

    Commit PZ

--- PZ_15/Library.py
+++ PZ_15/Library.py
@@ -44,31 +44,274 @@ with sq.connect('Library.sqlite') as con:
     REFERENCES authors ON DELETE CASCADE ON UPDATE CASCADE
     );""")
 
+with sq.connect('Library.sqlite') as con:
+    cur = con.cursor()
+    cur.executemany("INSERT INTO authors (surname, name) VALUES (?, ?);", info_authors)
+    cur.executemany("INSERT INTO sections (section) VALUES (?)", info_sections)
+    cur.executemany("INSERT INTO publishing (publish, city) VALUES (?, ?)", info_publishing)
+    cur.executemany("INSERT INTO author_book (id_book, id_author) VALUES (?, ?)", info_author_book)
+    cur.executemany("INSERT INTO books (name, year_publishing, storage, section, publish)"
+                    "VALUES (?, ?, ?, ?, ?)", info_books)
 
 with sq.connect('Library.sqlite') as con:
     cur = con.cursor()
-    cur.executemany("INSERT INTO authors VALUES (?, ?, ?)", info_authors)
-    cur.executemany("INSERT INTO sections VALUES (?)", info_sections)
-    cur.executemany("INSERT INTO publishing VALUES (?, ?)", info_publishing)
-    cur.executemany("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?)", info_books)
-    cur.executemany("INSERT INTO author_book VALUES (?, ?, ?)", info_author_book)
+
+    cur.execute("SELECT name, year_publishing FROM books ORDER BY year_publishing")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0], row[1])
+
+    author_last_name = 'Иванов'
+    author_first_name = 'Петр'
+    cur.execute("""SELECT books.name FROM books JOIN author_book ON books.id_books = author_book.id_book
+        JOIN authors ON author_book.id_author = authors.id_author WHERE authors.surname = ? AND authors.name = ?""",
+                (author_last_name, author_first_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0])
+
+    section = 'Детективы'
+    cur.execute("""SELECT name FROM books JOIN main.sections ON books.section = sections.section
+    WHERE sections.section = ?""",
+                (section,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0])
+
+    publisher = 'Eksmo'
+    cur.execute("""SELECT name FROM books JOIN publishing ON books.publish = publishing.publish
+    WHERE publishing.publish = ?""",
+                (publisher,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0])
+
+    cur.execute("SELECT surname, name FROM authors ORDER BY surname, name")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0], row[1])
+
+    cur.execute("SELECT name, year_publishing FROM books ORDER BY name, year_publishing")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0], row[1])
+
+    author_last_name = 'Иванов'
+    author_first_name = 'Петр'
+    cur.execute("""SELECT books.name, year_publishing FROM books JOIN
+    main.author_book ON books.id_books = author_book.id_book JOIN authors ON author_book.id_author = authors.id_author
+    WHERE authors.surname = ? AND authors.name = ? ORDER BY year_publishing""",
+                (author_last_name, author_first_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0], row[1])
+
+    year = 2017
+    cur.execute("SELECT name FROM books WHERE year_publishing = ?", (year,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0])
+
+    publisher = 'Eksmo'
+    cur.execute("""SELECT DISTINCT authors.surname, authors.name FROM authors JOIN
+    main.author_book ON authors.id_author = author_book.id_author JOIN books ON author_book.id_book = books.id_books
+    JOIN publishing ON books.publish = publishing.publish WHERE publishing.publish = ?""",
+                (publisher,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0], row[1])
+
+    word = '%метро%'
+    cur.execute("SELECT name FROM books WHERE name LIKE ?", (word,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0])
 
 
-with sq.connect('Library.sqlite') as con:
-    cur = con.cursor()
-    print('1)', *cur.execute("SELECT name, year_publishing FROM books ORDER BY year_publishing"), sep='\n')
-    print()
-    print('2)', *cur.execute("""SELECT name FROM author_book INNER JOIN
-    books ON books.id_books = author_book.id_author_book WHERE id_author == 1
-    """), sep='\n')
-    print()
-    # print('3)', *cur.execute("SELECT * FROM books"), sep='\n')
-    # print()
-    print('4)', *cur.execute("SELECT surname FROM authors ORDER BY surname"), sep='\n')
-    print()
-    # print('5)', *cur.execute("SELECT * FROM books"), sep='\n')
-    # print()
+# UPDATE Книги
+# SET год_издания = 2022
+# WHERE автор IN (
+# SELECT id_автора
+# FROM Авторы
+# WHERE фамилия = 'Иванов'
+# );
+#
+# UPDATE Книги
+# SET название = 'Новая книга', год_издания = 2023
+# WHERE место_хранения IN (
+# SELECT id_места
+# FROM МестаХранения
+# WHERE город = 'Москва'
+# );
+#
+# UPDATE Книги
+# SET название = 'Новое название', раздел = 'Фантастика'
+# WHERE автор IN (
+# SELECT id_автора
+# FROM Авторы
+# WHERE имя = 'Александр' AND фамилия = 'Петров'
+# );
+#
+# UPDATE Книги
+# SET название = 'Старое название'
+# WHERE год_издания BETWEEN 2010 AND 2015;
+#
+# UPDATE Книги
+# SET место_хранения = 'Библиотека №2'
+# WHERE автор = 7;
+#
+# UPDATE Книги
+# SET город_издания = (
+# SELECT город
+# FROM Издательства
+# WHERE Издательства.id_города = Книги.id_города
+# );
+#
+# UPDATE АвторКниги
+# SET код_автора = (
+# SELECT id_автора
+# FROM Авторы
+# WHERE Авторы.id_автора = АвторКниги.id_автора
+# );
+#
+# UPDATE Книги
+# SET раздел = (
+# SELECT название
+# FROM Разделы
+# WHERE Разделы.id_раздела = Книги.id_раздела
+# );
+#
+# UPDATE Книги
+# SET год_издания = (
+# SELECT год_издания
+# FROM АвторКниги
+# WHERE АвторКниги.id_книги = Книги.id_книги
+# );
+#
+# UPDATE Книги
+# SET место_хранения = (
+# SELECT МестаХранения.название
+# FROM Издательства
+# INNER JOIN МестаХранения ON Издательства.id_места = МестаХранения.id_места
+# WHERE Издательства.id_издательства = Книги.id_издательства
+# );
+#
+# UPDATE Авторы
+# SET фамилия = (
+# SELECT фамилия
+# FROM АвторКниги
+# WHERE АвторКниги.id_автора = Авторы.id_автора
+# )
+# WHERE id_автора IN (
+# SELECT id_автора
+# FROM АвторКниги
+# );
+#
+# UPDATE Книги
+# SET год_издания = 2022
+# WHERE город_издания = 'Москва';
+# UPDATE Книги
+# SET МестоХранения = 'Книжный шкаф 1'
+# WHERE АвторКниги IN (
+#   SELECT Код
+#   FROM АвторКниги
+#   WHERE АвторКниги.КодАвтора IN (
+#     SELECT Код
+#     FROM Авторы
+#     WHERE Авторы.Фамилия = 'Иванов'
+#   )
+# );
+#
+# UPDATE Книги
+# SET ГодИздания = 2023
+# WHERE АвторКниги IN (
+#   SELECT Код
+#   FROM АвторКниги
+#   WHERE АвторКниги.КодАвтора IN (
+#     SELECT Код
+#     FROM Авторы
+#     WHERE Авторы.Имя = 'Анна'
+#   )
+# );
+#
+# UPDATE Книги
+# SET Раздел = 'Классика'
+# WHERE ГородИздания = 'Санкт-Петербург';
+# UPDATE Книги
+# SET ГодИздания = 2024
+# WHERE АвторКниги IN (
+#   SELECT Код
+#   FROM АвторКниги
+#   WHERE АвторКниги.КодАвтора IN (
+#     SELECT Код
+#     FROM Авторы
+#     WHERE Авторы.Фамилия = 'Петров'
+#   )
+# );
 
-with sq.connect('Library.sqlite') as con:
-    cur = con.cursor()
-    cur.execute("""UPDATE books SET year_publishing = 2023, name = 'Новая книга' WHERE city = 'Москва'""")
+# 1. UPDATE Library SET year_published = 2022 WHERE author_lastname = 'Иванов';
+# 2. UPDATE Library SET title = 'Новая книга', year_published = 2023 WHERE location = 'Москва';
+# 3. UPDATE Library SET title = 'Новое название', section = 'Фантастика'
+# WHERE author_firstname = 'Александр' AND author_lastname = 'Петров';
+# 4. UPDATE Library SET title = 'Старое название' WHERE year_published BETWEEN 2010 AND 2015;
+# 5. UPDATE Library SET location = 'Библиотека №2' WHERE author_code = 7;
+# 6. UPDATE Книги SET city = (SELECT city FROM Издательства WHERE Издательства.city_code = Книги.city_code);
+# 7. UPDATE АвторКниги SET author_code = (SELECT code FROM Авторы WHERE Авторы.author_code = АвторКниги.author_code);
+# 8. UPDATE Книги SET section = (SELECT section FROM Разделы WHERE Разделы.section_name = Книги.section);
+# 9. UPDATE Книги SET year_published = (SELECT year_published FROM АвторКниги WHERE АвторКниги.book_code = Книги.code);
+# 10. UPDATE Книги SET location = (SELECT location FROM Издательства WHERE Издательства.name = Книги.publisher);
+# 11. UPDATE Авторы SET author_lastname = (SELECT author_lastname FROM АвторКниги
+# WHERE АвторКниги.author_code = Авторы.code);
+# 12. UPDATE Library SET year_published = 2022 WHERE location = 'Москва';
+# 13. UPDATE Library SET location = 'Книжный шкаф 1' WHERE author_lastname = 'Иванов';
+# 14. UPDATE Library SET year_published = 2023 WHERE author_firstname = 'Анна';
+# 15. UPDATE Library SET section = 'Классика' WHERE location = 'Санкт-Петербург';
+# 16. UPDATE Library SET year_published = 2024 WHERE author_lastname = 'Петров';
+
+
+# DELETE FROM Книги WHERE Раздел = 'Фантастика';
+
+# DELETE FROM Книги WHERE ГодИздания < 2000;
+
+# DELETE FROM АвторКниги WHERE КодАвтора = 1;
+
+# DELETE FROM Авторы WHERE Фамилия LIKE 'А%';
+# -- также нужно удалить связанные записи из таблицы АвторКниги
+
+# DELETE FROM АвторКниги WHERE КодАвтора IN (SELECT КодАвтора FROM Авторы WHERE Фамилия LIKE 'А%');
+
+# DELETE FROM Издательства WHERE Город = 'Москва';
+
+# DELETE FROM АвторКниги WHERE КодКниги = 10;
+
+# DELETE FROM Книги WHERE МестоХранения = 'Склад';
+
+# DELETE FROM Разделы WHERE Раздел = 'Детективы';
+
+# DELETE FROM АвторКниги WHERE КодАвтора = 2;
+
+# DELETE FROM Издательства WHERE Издательство = 'O''Reilly Media';
+
+# DELETE FROM Книги WHERE Название LIKE '%Война%';
+
+# DELETE FROM Книги WHERE ГодИздания <= 2000 AND МестоХранения = 'Библиотека №1';
+
+# DELETE FROM Авторы WHERE КодАвтора NOT IN (SELECT КодАвтора FROM АвторКниги);
+
+# DELETE FROM Книги WHERE КодИздательства IN (SELECT КодИздательства FROM Издательства WHERE Город = 'Москва');
+
+# DELETE FROM Авторы WHERE Фамилия LIKE 'А%';
+# -- также нужно удалить связанные записи из таблицы АвторКниги
+
+# DELETE FROM АвторКниги WHERE КодАвтора IN (SELECT КодАвтора FROM Авторы WHERE Фамилия LIKE 'А%');
+
+# DELETE FROM АвторКниги WHERE КодКниги IN (SELECT КодКниги FROM Книги WHERE КодИздательства IN
+# (SELECT КодИздательства FROM Издательства WHERE Город = 'Москва'));
+
+# DELETE FROM Книги WHERE Код IN (SELECT КодКниги FROM АвторКниги JOIN
+# Авторы ON АвторКниги.КодАвтора = Авторы.КодАвтора WHERE Авторы.Фамилия LIKE 'П%');
+
+# DELETE FROM Книги WHERE КодИздательства IN (SELECT КодИздательства FROM Издательства WHERE Название LIKE 'Н%');
+
+# DELETE FROM АвторКниги WHERE КодКниги IN (SELECT Код FROM Книги WHERE КодИздательства IN
+# (SELECT КодИздательства FROM Издательства WHERE Название LIKE 'Н%'));
--- PZ_15/data.py
+++ PZ_15/data.py
@@ -1,63 +1,71 @@
 info_authors = [
-    (1, 'Пушкин', 'Александр'),
-    (2, 'Толстой', 'Лев'),
-    (3, 'Булгаков', 'Михаил'),
-    (4, 'Гоголь', 'Николай'),
-    (5, 'Тургенев', 'Иван'),
-    (6, 'Иванов', 'Иван'),
-    (7, 'Анисимова', 'Анна'),
-    (8, 'Достоевский', 'Федор'),
-    (9, 'Петров', 'Александр'),
-    (10, 'Чехов', 'Антон')
+    ('Dostoevsky', 'Fyodor'),
+    ('Tolstoy', 'Leo'),
+    ('Pushkin', 'Alexander'),
+    ('Gogol', 'Nikolai'),
+    ('Chekhov', 'Anton'),
+    ('Goncharov', 'Ivan'),
+    ('Lermontov', 'Mikhail'),
+    ('Turgenev', 'Ivan'),
+    ('Bulgakov', 'Mikhail'),
+    ('Pasternak', 'Boris')
 ]
 
 info_books = [
-    (1, 'Евгений Онегин', 1903, 'Книжный шкаф 1', 'Фантастика', 1),
-    (2, 'Война и мир', 1892, 'Склад', 'Классика', 2),
-    (3, 'Пиковая дама', 1887, 'Библиотека №1', 'Детективы', 1),
-    (4, 'Ночь перед Рождеством', 1900, 'Книжный шкаф 2', 'Классика', 4),
-    (5, 'Мастер и Маргарита', 1898, 'Библиотека №2', 'Детективы', 3),
-    (6, 'Дубровский', 1896, 'Склад', 'Классика', 1),
-    (7, 'Мертвые души', 1898, 'Книжный шкаф 3', 'Фантастика', 4),
-    (8, 'Капитанская дочка', 1883, 'Склад', 'Классика', 1),
-    (9, 'Муму', 1896, 'Библиотека №3', 'Детективы', 5),
-    (10, 'Идиот', 1889, 'Склад', 'Классика', 8)
+    ('War and Peace', 1869, 'Library', 'Fiction', 'Penguin Random House'),
+    ('Crime and Punishment', 1866, 'Library', 'Fiction', 'Hachette Livre'),
+    ('Anna Karenina', 1878, 'Library', 'Fiction', 'HarperCollins'),
+    ('Dead Souls', 1842, 'Library', 'Fiction', 'Wolters Kluwer'),
+    ('The Cherry Orchard', 1904, 'Library', 'Drama', 'Oxford University Press'),
+    ('The Brothers Karamazov', 1880, 'Library', 'Fiction', 'Cambridge University Press'),
+    ('Doctor Zhivago', 1957, 'Library', 'Fiction', 'Bloomsbury Publishing'),
+    ('Fathers and Sons', 1862, 'Library', 'Fiction', 'Pearson Education'),
+    ('Master and Margarita', 1967, 'Library', 'Fiction', 'Cengage Learning'),
+    ('The Queen of Spades', 1834, 'Library', 'Mystery', 'Elsevier')
 ]
 
 info_sections = [
-    ('Фантастика',),
-    ('Детективы',),
-    ('Прозы',),
-    ('Драмы',),
-    ('Комедии',),
-    ('Трагедии',),
-    ('Элегии',),
-    ('Оды',),
-    ('Сказки',),
+    ('Fiction',),
+    ('Non-fiction',),
+    ('Science Fiction',),
+    ('Poetry',),
+    ('Drama',),
+    ('Romance',),
+    ('Thriller',),
+    ('Mystery',),
+    ('Horror',),
+    ('Biography',)
 ]
 
 info_publishing = [
-    ("O'Reilly Media", 'Москва'),
-    ('Феникс', 'Санкт-Петербург'),
-    ('Омега-Принт', 'Ростов-на-Дону'),
-    ('Гинго', 'Москва'),
-    ('Антей', 'Нижний Новгород'),
-    ('Айрис-пресс', 'Ростов-на-Дону'),
-    ('Евро Медиа', 'Москва'),
-    ('Театральный вестник', 'Нижний Новгород'),
-    ('Проф-Пресс', 'Ростов-на-Дону'),
-    ('Альфа-книга', 'Москва')
+    ('Penguin Random House', 'New York'),
+    ('Hachette Livre', 'Paris'),
+    ('HarperCollins', 'London'),
+    ('Wolters Kluwer', 'Amsterdam'),
+    ('Oxford University Press', 'Oxford'),
+    ('Cambridge University Press', 'Cambridge'),
+    ('Bloomsbury Publishing', 'London'),
+    ('Pearson Education', 'London'),
+    ('Cengage Learning', 'Boston'),
+    ('Elsevier', 'Amsterdam')
 ]
 
 info_author_book = [
-    (1, 5, 10),
-    (2, 1, 8),
-    (3, 7, 6),
-    (4, 1, 3),
-    (5, 1, 1),
-    (6, 1, 6),
-    (7, 9, 3),
-    (8, 4, 7),
-    (9, 6, 1),
-    (10, 10, 1)
+    (1, 2),
+    (1, 3),
+    (2, 1),
+    (2, 2),
+    (3, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 2),
+    (6, 3),
+    (6, 6),
+    (7, 10),
+    (8, 9),
+    (9, 9),
+    (9, 10),
+    (10, 3),
+    (10, 7)
 ]
